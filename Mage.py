0485a929b35237318751f2212167373d5c0185ef
Aliasghar Rashidabadi <farzad.rashidabadi0785@gmail.com>
2026-08-10 16:46:09 +0330

    Added status attribute.

--- Mage.py
+++ Mage.py
@@ -1,12 +1,17 @@
-from random import randint
+#from random import randint
 
 class Mage:
-    def __init__(self, name, house_obj = None, hp=100, mana=50):
+    def __init__(self, name, house_obj = None, hp=100, mana=50, crit_chance=0.1, level=1, xp=0):
         self.name = name
         self.hp = hp
         self.mana = mana
         self.coins = 10000
+        self._crit_chance = crit_chance
         self.house = house_obj
+        self.level = level
+        self.xp = xp
+
+        self._status = {}
 
     @property
     def name(self):
@@ -14,6 +19,7 @@ class Mage:
     @name.setter
     def name(self, new_n):
         self._name = new_n.title()
+        self.add_status("Name", self.name)
 
     @property
     def hp(self):
@@ -23,6 +29,7 @@ class Mage:
         self._hp = amount
         if self.hp < 0:
             self._hp = 0
+        self.add_status("HP", self.hp)
 
     @property
     def mana(self):
@@ -32,6 +39,7 @@ class Mage:
         self._mana = amount
         if self.mana < 0:
             self._mana = 0
+        self.add_status("Mana", self.mana)
 
     @property
     def coins(self):
@@ -39,6 +47,9 @@ class Mage:
     @coins.setter
     def coins(self, amount):
         self._coins = amount
+        if self.coins < 0:
+            self.coins = 0
+        self.add_status("Coins", self.coins)
 
     @property
     def house(self):
@@ -46,3 +57,27 @@ class Mage:
     @house.setter
     def house(self, new_house):
         self._house = new_house
+        self.add_status("House", self._house.name)
+
+    @property
+    def level(self):
+        return self._level
+    @level.setter
+    def level(self, new_l):
+        self._level = new_l
+        self.add_status("Level", self.level)
+
+    @property
+    def xp(self):
+        return self._xp
+    @xp.setter
+    def xp(self, new_xp):
+        self._xp = new_xp
+        self.add_status("XP", self.xp)
+
+    @property
+    def status(self):
+        return self._status
+
+    def add_status(self, att_name, att_amount):
+        self._status[att_name] = att_amount
