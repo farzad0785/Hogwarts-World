9785e1e67b607aef1f1d7e8fbd2e9f1e2b765993
Aliasghar Rashidabadi <farzad.rashidabadi0785@gmail.com>
2026-08-19 10:58:14 +0330

    Added __str__ dunder method.

--- House.py
+++ House.py
@@ -1,9 +1,10 @@
 class House:
+    house = []
     def __init__(self, quote, name, buffs):
         self._quote = quote
         self._name = name
         self._buffs = buffs
-
+        House.house.append(self)
     #Encapsulation
     @property
     def quote(self):
@@ -17,8 +18,11 @@ class House:
     def buffs(self):
         return self._buffs
 
-
     def add_buffs(self):
         for buff, amount in self.buffs.items():
             if amount != 0:
                 pass
+
+    def __str__(self):
+        return (f"{self.name:<20}{self.buffs['hp']:<16}{self.buffs['mana']:<15}{self.buffs['coins']:<18}"
+                f"{self.buffs['crit chance']}")
